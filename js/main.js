51d60c284dc9fc76a0305bfc9ad102bd083cc1f4
CarloSalaris <carloa.salaris@gmail.com>
2023-05-02 14:45:02 +0200

    Comment structure JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+// CONSEGNA
+
+//Scrivi un programma che stampi in console i numeri da 1 a 100
+    //Per i multipli di 3 stampa “Fizz” al posto del numero
+    //Per i multipli di 5 stampa “Buzz” al posto del numero
+    //Per i numeri che sono sia multipli di 3 che di 5 stampa “FizzBuzz”
+
+        // "N Multiplo di x:" = N % x === 0
+
+// FOR LOOP (per ogni numero compreso tra 1 e 100)
+    // SE il numero è CONTEMPORANEAMENTE multiplo di 3 e di 5 --> stampa “FizzBuzz”
+    // ALTRIMENTI SE il numero è multiplo di 3 --> stampa “Fizz”
+    // ALTRIMENTI SE il numero è multiplo di 5 --> stampa “Buzz”
+    // ALTRIMENTI stampa il NUMERO
